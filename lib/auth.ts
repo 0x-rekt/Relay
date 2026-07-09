a416b1ec6ef85406df0494cc5f02aa9a1cf80d77
0x-rekt <kolaysowdarjya@gmail.com>
2026-07-09 12:28:09 +0530

    feat: implement authentication using Better Auth with Google and GitHub providers

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,4 +6,15 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+  secret: process.env.BETTER_AUTH_SECRET as string,
 });
